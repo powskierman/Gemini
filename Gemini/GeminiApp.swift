cab235cc570f0f9a12033f8b3f5ad93a80dedd40
powskierman <mlapoint99@hotmail.com>
2024-02-28 10:48:52 -1000

    Initial Commit

---
+++ Gemini/GeminiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiApp.swift
+//  Gemini
+//
+//  Created by Michel Lapointe on 2024-02-28.
+//
+
+import SwiftUI
+
+@main
+struct GeminiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
