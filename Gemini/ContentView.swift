67d33ec3af62d8d86a141c999b7f1844c349ce09
powskierman <mlapoint99@hotmail.com>
2024-02-28 12:09:37 -1000

    Initial commit.  Modified from: https://medium.com/google-cloud/get-started-with-gemini-ai-on-ios-with-swiftui-5dc3d28c8e3a

--- Gemini/ContentView.swift
+++ Gemini/ContentView.swift
@@ -6,19 +6,72 @@
 //
 
 import SwiftUI
+import GoogleGenerativeAI
 
 struct ContentView: View {
+    let model = GenerativeModel(name: "gemini-pro", apiKey: APIKey.default)
+    @State var textInput = ""
+    @State var aiResponse: LocalizedStringKey = "Hello! How can I help you today?"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(.geminiLogo)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200)
+            
+            ScrollView {
+                Text(aiResponse)
+                    .font(.body)
+                    .multilineTextAlignment(.center)
+            }
         }
+        
+        HStack {
+            TextField("Enter a message", text: $textInput)
+                .textFieldStyle(.roundedBorder)
+                .foregroundStyle(.black)
+            Button(action: sendMessage, label: {
+                Image(systemName: "paperplane.fill")
+            })
+        }
+        
+        .foregroundStyle(.white)
         .padding()
+        .background {
+            ZStack {
+                Color.black
+            }
+            .ignoresSafeArea()
+        }
+    }
+    
+    func sendMessage() {
+        aiResponse = ""
+        
+        Task {
+            do {
+                let response = try await model.generateContent(textInput)
+                
+                guard let text = response.text else  {
+                    textInput = "Sorry, I could not process that.\nPlease try again."
+                    return
+                }
+                
+                textInput = ""
+                aiResponse = (.init(text))
+                
+            } catch {
+                aiResponse = "Something went wrong!\n\(error.localizedDescription)"
+            }
+        }
     }
 }
-
-#Preview {
-    ContentView()
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 12") // You can specify different devices here
+    }
 }
+#endif
